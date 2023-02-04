092e28fa58bbc9d1cc83a00f4c5cf545a55ae906
Pradyumna-Hegde <pradyumna.coder@gmail.com>
2023-02-04 23:39:39 +0530

    added Routes and <UserName /> component

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,10 +1,50 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import UserName from "./components/username";
+import Register from "./components/register";
+import Password from "./components/password";
+import Profile from "./components/profile";
+import Recovery from "./components/recovery";
+import Reset from "./components/reset";
+import PageNotFound from "./components/pagenotfound";
+
+// root routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <UserName />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/password",
+    element: <Password />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "/recovery",
+    element: <Recovery />,
+  },
+  {
+    path: "/reset",
+    element: <Reset />,
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+]);
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello Login Application</h1>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 };
 
